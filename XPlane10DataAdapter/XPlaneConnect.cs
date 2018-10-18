fbb01127b7f716e02cadb60ff92d4a52105615ab
Peefy <xpf6677@163.com>
2018-10-18 12:57:29 +0800

    Add xplane driver

---
+++ XPlane10DataAdapter/XPlaneConnect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPlane10DataAdapter
+{
+    public class XPlaneConnect
+    {
+    }
+
+    public enum ViewType
+    {
+
+    }
+
+    public enum WayPointOp
+    {
+
+    }
+}
